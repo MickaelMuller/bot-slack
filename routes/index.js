c7ce2825648344662bdb3eabf1a15b0d3536a935
Mickael Muller <micka.muller@cdbdx.biz>
2019-05-26 09:04:05 +0200

    test dtBase heroku

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,17 @@
 const { Router } = require('express');
 const request = require('request');
+const { Client } = require('pg');
+const Slack = require('slack')
+
+var client = new Client({
+  user: "zosgirralsfyqh",
+  password: process.env.passPG,
+  database: process.env.dataBaseName,
+  port: 5432,
+  host: process.env.hostPG,
+  ssl: true
+});
+client.connect();
 
 const router = Router();
 
@@ -18,6 +30,17 @@ router.post('/geekjoke', (req, res) => {
       channel: 'CHSSBBX5F',
       text: `${JSON.parse(body)}`,
     });
+  });
+});
+
+router.get('/testinsert', (req, res) => {
+  client.query('SELECT name from users', (err, results) => {
+    if (err) res.sendStatus(500);
+    return res.json({
+      response_type: 'in_channel',
+      channel: 'CHSSBBX5F',
+      text: `Les plus beau c'est ${results.rows.map(row => row.name).toString()}`
+    })
   });
 });
 
